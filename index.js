cdea2b37128ff6a18b19ba25d00b9a16bd4fa52e
Blaine Jester <bjester@servicescout.com>
2016-09-19 11:18:40 -0700

    [#129100095] Added purging

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+"use strict";
 console.log('Initializing: lambda-maxcdn-purge');
 
+// Imports
 const config = require('config');
-const maxcdn = require('maxcdn').create(
+const MaxCDN = require('maxcdn');
+const Purger = require('./lib/purger');
+
+// Instances
+const purger = new Purger(MaxCDN.create(
   config.get('maxcdn.company_alias'),
   config.get('maxcdn.key'),
   config.get('maxcdn.secret')
-);
+), config);
 
-module.exports.handler = function(event, context){};
+module.exports.handler = function(event, context)
+{
+  Promise.all(event.Records.map(function(record) 
+  { 
+    return purger.purge(record)
+      .then(
+        results => { console.log(`Successfully purged ${results.pop()}`) }, 
+        error => { console.error(error) }
+      ); 
+  }))
+    .then(
+      () => { context.succeed() }, 
+      () => { context.fail() }
+    );
+};
